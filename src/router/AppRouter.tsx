9c5d2a40addaf75aecf44fcb81352d6672b92ac6
Alex <jabs0506@hotmail.com>
2026-08-31 12:20:10 -0600

    Rutas anidadas de app y cuenta
    Se agregan AppLayout y AccountLayout con Outlet para las secciones internas y de visitante

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,14 +3,28 @@ import HomePage from "../pages/public/HomePage";
 import EventsPage from "../pages/public/EventsPage";
 import LoginPage from "../pages/public/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage.tsx";
+import AppLayout from "../layouts/AppLayout.tsx";
+import AccountLayout from "../layouts/AccountLayout.tsx";
+import BoxOfficePage from "../pages/app/BoxOfficePage.tsx";
+import ReservationsPage from "../pages/app/ReservationsPage.tsx";
+import EventsAdminPage from "../pages/app/EventsAdminPage.tsx";
+import MyReservationsPage from "../pages/account/MyReservationsPage.tsx";
 
 export default function AppRouter() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/eventos" element={<EventsPage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="*" element={<NotFoundPage />}></Route>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/eventos" element={<EventsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/app" element={<AppLayout />}>
+                <Route path="taquilla" element={<BoxOfficePage />} />
+                <Route path="reservaciones" element={<ReservationsPage />} />
+                <Route path="eventos" element={<EventsAdminPage />} />
+            </Route>
+            <Route path="/cuenta" element={<AccountLayout />}>
+                <Route path="reservaciones" element={<MyReservationsPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     )
 }
